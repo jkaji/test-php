dd448c6be9d1f3ff354d0f198565e549b9a7ce54
jkaji <kajigaki2021@gmail.com>
2025-07-08 12:16:39 +0900

    リモートリポジトリの変更

---
+++ src/php01/index04.php
@@ -0,0 +1,50 @@
+<?php
+$a = 15;
+$b = 3;
+$c = 10;
+
+$answer1 = $a + $b;
+$answer2 = $a - $b;
+$answer3 = $a * $b;
+$answer4 = $a / $b;
+$answer5 = $a % $C;
+
+echo $answer1;
+echo "<br />";
+echo $answer2;
+echo "<br />";
+echo $answer3;
+echo "<br />";
+echo $answer4;
+echo "<br />";
+echo $answer5;
+
+$a1 = 15;
+$b1 = 2;
+$c1 = 10;
+$d1 = 5;
+
+$a1 = $b1;
+$c1 += $d1;
+
+echo $a1;
+echo "<br />";
+echo $c1;
+echo "<br />";
+
+$a2 = 20;
+$b2 = 5;
+
+echo ($a2 > 10 && $a2 < 30);
+echo "<br />";
+
+$a3 = $b3 = 10;
+$c3 = $d3 = 5;
+
+echo ++$a3;
+echo "<br />";
+echo $b3++;
+echo "<br />";
+echo --$c3;
+echo "<br />";
+echo $d3--;
---
+++ src/php01/index06.php
@@ -0,0 +1,30 @@
+<?php
+for ($i = 0; $i <= 4; $i++) {
+    echo $i;
+    }
+echo '<br />';
+?>
+
+<?php
+for ($i = 1; $i <= 5; $i++) {
+    echo $i*2 . '<br />';
+}
+?>
+
+<?php
+$i = 0;
+
+while ($i < 3) {
+    echo 'i = ' . $i . '<br />';
+    $i += 1;  
+}
+?>
+
+<?php
+$count = 0;
+
+while ($count < 20) {
+    $count += 1;
+    echo $count . '<br />';
+}
+?>
